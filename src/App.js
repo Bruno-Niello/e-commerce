13ca5cf431ee1a4a7ca5d89ea8227c71b7b2319e
Bruno Niello <brunoniello@gmail.com>
2022-05-31 13:42:35 -0300

    trabajo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import React, {useState} from 'react';
 import '../src/styles/App.css';
 import NavBar from './components/NavBar';
+import Test from './components/Test';
 import ListadoContainer from './components/ItemListContainer';
 
 function App() {
+  const [estaMontado, setEstaMontado] = useState(true);
   return (
     <div className="App grid-container">
       <NavBar />
       <div class="home">
         <ListadoContainer />
       </div>
+      {estaMontado ? <Test desde={0}/> : 'dismount!'}
+      <button onClick={()=>{setEstaMontado(false);}}>Sacar test</button>
+      <button onClick={()=>{setEstaMontado(true);}}>Mostrar test</button>
     </div>
     
   );
